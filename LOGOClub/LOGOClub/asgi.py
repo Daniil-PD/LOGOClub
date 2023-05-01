bcd3fd5aa83213c52b11feeb3c9b646a4b117109
Daniil-PD <d576765765@gmail.com>
2023-05-01 15:51:49 +0400

    Создание структуры проекта

---
+++ LOGOClub/LOGOClub/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for LOGOClub project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LOGOClub.settings')
+
+application = get_asgi_application()
